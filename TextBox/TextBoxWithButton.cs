9d2f1d49ccd3cb636d6987cb5269ddd55670cbd5
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-02 10:53:57 +0900

    実装ファイルの仮作成

---
+++ TextBox/TextBoxWithButton.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel;
+
+namespace  WinFormsControl  {
+
+public partial class TextBoxWithButton : UserControl
+{
+    public TextBoxWithButton()
+    {
+        InitializeComponent();
+    }
+
+}   //  End of partial class  TextBoxWithButton
+
+}   //  End of namespace  WinFormsControl
---
+++ Utils/RectSelectForm.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel;
+
+namespace  WinFormsControl  {
+
+public partial class RectSelectForm : UserControl
+{
+    public RectSelectForm()
+    {
+        InitializeComponent();
+    }
+
+}   //  End of partial class  RectSelectForm
+
+}   //  End of namespace  WinFormsControl
