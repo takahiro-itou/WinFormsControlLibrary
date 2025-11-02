78e975c4f9142932ca5e68a3f82c83b87a35d4ca
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-02 11:06:02 +0900

    イベントハンドラを実装

--- TextBox/TextBoxWithButton.cs
+++ TextBox/TextBoxWithButton.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.ComponentModel;
 
 namespace  WinFormsControl  {
@@ -17,7 +18,7 @@ public partial class TextBoxWithButton : UserControl
       , Description("ボタンに表示する文字列")
       , Category("表示")
     ]
-    public String ButtonText
+    public System.String ButtonText
     {
         get { return  btnRefer.Text; }
         set { btnRefer.Text = value; }
@@ -30,7 +31,7 @@ public partial class TextBoxWithButton : UserControl
       , Description("ラベルに表示する文字列")
       , Category("表示")
     ]
-    public String LabelCaption
+    public System.String LabelCaption
     {
         get { return  lblCaption.Text; }
         set { lblCaption.Text = value; }
@@ -43,10 +44,42 @@ public partial class TextBoxWithButton : UserControl
       , Description("テキストボックスに入力された文字列")
       , Category("表示")
     ]
-    public String Text
+    public System.String Text
     {
         get { return  txtEdit.Text; }
         set { txtEdit.Text = value; }
+    }
+
+    //----------------------------------------------------------------
+    /**   ButtonClick イベント
+    **
+    **/
+    [Browsable(true)
+      , Description("ボタンがクリックされた時に発生します")
+      , Category("アクション")
+    ]
+    public event System.EventHandler<EventArgs>? ButtonClick;
+
+    //----------------------------------------------------------------
+    /**   イベントハンドラを呼び出すメソッド
+    **
+    **/
+    protected virtual void OnReferButtonClick(EventArgs e)
+    {
+        var eventHandler = ButtonClick;
+        if ( eventHandler != null ) {
+            eventHandler(this, e);
+        }
+    }
+
+    //----------------------------------------------------------------
+    /**   イベントハンドラ
+    **
+    **    ボタンのクリックイベント
+    **/
+    private void btnRefer_Click(object sender, EventArgs e)
+    {
+        OnReferButtonClick(e);
     }
 
 }   //  End of partial class  TextBoxWithButton
