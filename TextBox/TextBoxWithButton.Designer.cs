af3ee08f759f3574e9b5f62c1fbb0ab0e02397b4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-02 10:46:09 +0900

    ユーザーコントロールをデザイン

---
+++ TextBox/TextBoxWithButton.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WinFormsControl
+{
+    partial class TextBoxWithButton
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(TextBoxWithButton));
+
+            lblCaption = new Label();
+            txtEdit = new TextBox();
+            btnRefer = new Button();
+            this.SuspendLayout();
+
+            //
+            // lblCaption
+            //
+            resources.ApplyResources(lblCaption, "lblCaption");
+            lblCaption.Name = "lblCaption";
+            //
+            // txtEdit
+            //
+            resources.ApplyResources(txtEdit, "txtEdit");
+            txtEdit.Name = "txtEdit";
+            //
+            // btnRefer
+            //
+            resources.ApplyResources(btnRefer, "btnRefer");
+            btnRefer.Name = "btnRefer";
+            btnRefer.UseVisualStyleBackColor = true;
+            btnRefer.Click += btnRefer_Click;
+
+            //
+            // TextBoxWithButton
+            //
+            AutoScaleMode = AutoScaleMode.None;
+            this.Controls.Add(btnRefer);
+            this.Controls.Add(txtEdit);
+            this.Controls.Add(lblCaption);
+            resources.ApplyResources(this, "$this");
+            this.Name = "TextBoxWithButton";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblCaption;
+        private TextBox txtEdit;
+        private Button btnRefer;
+    }
+}
